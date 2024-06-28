f78d409f3de2e66b736fdb18d53e9baf15cf70e7
ANSON21VV <anderosncirino@gmail.com>
2024-06-27 21:57:29 -0300

    brincando

---
+++ oi.js
@@ -0,0 +1,9 @@
+let xp = 1000; // experiência inicial
+let metaXP = 10000; // experiência desejada
+
+for (let i = 1; xp <= metaXP; i++) {
+    xp += i * 1000;
+    console.log("Experiência atual: " + xp);
+}
+
+console.log("Meta de experiência alcançada!");
